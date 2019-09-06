d38a4ea6fd1b0ce9144439f4796d77d2a2a5b928
Khaled Ayed <ayed.khaled@gmail.com>
2019-09-06 14:16:19 +0100

    integrate backend

--- src/pages/appareils/appareils.ts
+++ src/pages/appareils/appareils.ts
@@ -1,27 +1,36 @@
-import { Component } from '@angular/core';
-import { ModalController, MenuController, NavController } from 'ionic-angular';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ModalController, MenuController, NavController, ToastController, LoadingController } from 'ionic-angular';
 import { SingleAppareilPage } from './single-appareil/single-appareil';
 import { Appareil } from '../../models/Appareil';
 import { AppareilsService } from '../../services/appareils.service';
 import { AppareilFormPage } from '../appareil-form/appareil-form';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'page-appareils',
   templateUrl: 'appareils.html'
 })
 
-export class AppareilsPage {
+export class AppareilsPage implements OnInit, OnDestroy{
   
 appareilsList: Appareil[];
+appareilsSubscription: Subscription;
 
 constructor(private modalCtrl: ModalController,
             private appareilsService: AppareilsService,
             private menuCtrl: MenuController,
-            private navCntrl: NavController){
+            private navCntrl: NavController,
+            private toastCtrl: ToastController,
+            private loadingCtrl: LoadingController){
 
 }
-ionViewWillEnter(){
-  this.appareilsList = this.appareilsService.appareilsList.slice();
+ngOnInit(){
+  this.appareilsSubscription = this.appareilsService.appareils$.subscribe(
+    (appareils: Appareil[]) => {
+      this.appareilsList = appareils;
+    }
+  );
+  this.appareilsService.emitAppareils();
 }
 
 onLoadAppareil(index: number){
@@ -35,5 +44,62 @@ onToggleMenu(){
 
 onNewAppareil(){
   this.navCntrl.push(AppareilFormPage);
+}
+onSaveList(){
+  let loader = this.loadingCtrl.create({
+    content: 'Sauvegarde en cours...'
+  });
+  loader.present();
+  this.appareilsService.saveData().then(
+    ()=> {
+      loader.dismiss();
+      this.toastCtrl.create({
+        message: 'Données sauvegardées !',
+        duration: 3000,
+        position: 'bottom'
+      }).present();
+    }
+  ).catch(
+    (error) => {
+      loader.dismiss();
+      this.toastCtrl.create({
+        
+        message: error,
+        duration: 3000,
+        position: 'bottom'
+      }).present();
+    }
+  );
+}
+
+onFetchList(){
+  let loader = this.loadingCtrl.create({
+    content: 'Récupération en cours...'
+  });
+  loader.present();
+  this.appareilsService.retrieveData().then(
+    ()=> {
+      loader.dismiss();
+      this.toastCtrl.create({
+        message: 'Données récupérées !',
+        duration: 3000,
+        position: 'bottom'
+      }).present();
+    }
+  ).catch(
+    (error) => {
+      loader.dismiss();
+      this.toastCtrl.create({
+        
+        message: error,
+        duration: 3000,
+        position: 'bottom'
+      }).present();
+    }
+  );
+}
+
+ngOnDestroy(){
+ this.appareilsSubscription.unsubscribe(); 
 }
 }
--- src/services/appareils.service.ts
+++ src/services/appareils.service.ts
@@ -1,6 +1,13 @@
 import { Appareil} from '../models/Appareil';
+import { Subject } from 'rxjs/Subject';
+import * as firebase from 'firebase';
+import DataSnapshot = firebase.database.DataSnapshot;
 
 export class AppareilsService{
+
+
+  appareils$ = new Subject<Appareil[]>();
+
     appareilsList: Appareil[] = [
         {
           name: 'Machine à laver',
@@ -37,5 +44,39 @@ export class AppareilsService{
         
   addAppareil(appareil: Appareil){
     this.appareilsList.push(appareil);
+    this.emitAppareils();
   }
+
+  emitAppareils(){
+    this.appareils$.next(this.appareilsList.slice());
+  }
+
+saveData(){
+return new Promise((resolve, reject) =>{
+  firebase.database().ref('appareils').set(this.appareilsList).then(
+    (data: DataSnapshot) => {
+      resolve(data);
+    }
+  ).catch(
+    (error) =>{
+      reject(error);
+    }
+  );
+});
+}
+retrieveData(){
+return new Promise((resolve, reject) => {
+  firebase.database().ref('appareils').once('value').then(
+    (data: DataSnapshot) => {
+      this.appareilsList = data.val();
+      this.emitAppareils();
+      resolve('Données récupérées avec succés !');
+    }
+  ).catch(
+    (error) => {
+      reject(error);
+    }
+  );
+});
+}
 }
